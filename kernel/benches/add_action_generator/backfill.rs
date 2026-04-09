ab8839b71e9896a2f6266732f44adec30b76141a
Fokko Driesprong <fokko@apache.org>
2026-04-09 12:57:50 +0200

    Revert unneeded changes

--- kernel/benches/add_action_generator/backfill.rs
+++ kernel/benches/add_action_generator/backfill.rs
@@ -1000,7 +1000,7 @@ fn partition_actions_into_leaves(
     let mut actions_in_current_leaf: usize = 0;
     let mut leaf_count: usize = 0;
 
-    txn.with_manifest_commit();
+    let mc = txn.with_manifest_commit();
 
     // Scan metadata and count actions
     let scan_iter = scan.scan_metadata(engine)?;
@@ -1031,14 +1031,14 @@ fn partition_actions_into_leaves(
         if actions_in_current_leaf > 0 && actions_in_current_leaf + selected_count > batch_size {
             // Finish the current leaf before adding this batch
             let leaf_result = current_leaf_writer.take().unwrap().finish(engine)?;
-            txn.add_leaf(leaf_result)?;
+            mc.add_leaf(leaf_result)?;
             leaf_count += 1;
             actions_in_current_leaf = 0;
         }
 
         // Add this batch to the current (or new) leaf
         if current_leaf_writer.is_none() {
-            current_leaf_writer = Some(txn.new_leaf_node_writer(engine)?);
+            current_leaf_writer = Some(mc.new_leaf_node_writer(engine)?);
         }
 
         let leaf_writer = current_leaf_writer.as_mut().unwrap();
@@ -1048,7 +1048,7 @@ fn partition_actions_into_leaves(
         // If we've reached or exceeded batch_size, finish this leaf
         if actions_in_current_leaf >= batch_size {
             let leaf_result = current_leaf_writer.take().unwrap().finish(engine)?;
-            txn.add_leaf(leaf_result)?;
+            mc.add_leaf(leaf_result)?;
             leaf_count += 1;
             actions_in_current_leaf = 0;
 
@@ -1060,7 +1060,7 @@ fn partition_actions_into_leaves(
 
     // Finish any remaining leaf
     if let Some(writer) = current_leaf_writer {
-        txn.add_leaf(writer.finish(engine)?)?;
+        mc.add_leaf(writer.finish(engine)?)?;
         leaf_count += 1;
     }
 
