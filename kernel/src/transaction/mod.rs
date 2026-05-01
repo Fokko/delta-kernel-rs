12d12aa9d1c79ff9ff10e486d1aaf13f7eec5aa4
Fokko Driesprong <fokko@apache.org>
2026-05-01 08:58:55 +0200

    Less is more

--- kernel/src/transaction/mod.rs
+++ kernel/src/transaction/mod.rs
@@ -7,8 +7,6 @@ use std::sync::{Arc, LazyLock, OnceLock};
 use delta_kernel_derive::internal_api;
 use tracing::{info, instrument};
 
-#[cfg(feature = "iceberg-nativev4")]
-use crate::actions::get_log_domain_metadata_schema;
 use crate::actions::{
     as_log_add_schema, get_commit_schema, get_log_checkpoint_action_schema,
     get_log_domain_metadata_schema, get_log_remove_schema, get_log_txn_schema, CheckpointAction,
