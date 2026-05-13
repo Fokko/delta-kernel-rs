c69bb2f3c7d343b4dc5001822b0f234e43c3b96e
Amogh Jahagirdar <amoghj@apache.org>
2026-05-13 11:59:22 -0600

    fix: TrackingStatus:EXISTED should be TrackingStatus:EXISTING

--- kernel/src/content_tree/reader.rs
+++ kernel/src/content_tree/reader.rs
@@ -110,7 +110,7 @@ fn visit_metadata_entry_at<'a>(
     // Extract tracking fields
     let tracking_status_int: i32 = getters[3].get(row_index, "tracking.status")?;
     let tracking_status = match tracking_status_int {
-        0 => TrackingStatus::Existed,
+        0 => TrackingStatus::Existing,
         1 => TrackingStatus::Added,
         2 => TrackingStatus::Deleted,
         3 => TrackingStatus::Replaced,
